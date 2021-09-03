5c869a33c74943f8fb4d5f17f008b27dc5a94a6c
odahyl64 <odahyl2004@hotmail.com>
2021-09-03 20:50:58 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BD_USER', 'magicdes_marcelo'); // USE O TEU USUÁRIO DE BANCO DE DADOS
+define('BD_PASS', 'aiy3papa'); // USE A TUA SENHA DO BANCO DE DADOS
+define('BD_NAME', 'magicdes_promicnet'); // USE O NOME DO TEU BANCO DE DADOS
+
+mysql_connect('cpmy0003.servidorwebfacil.com', BD_USER, BD_PASS);
+mysql_select_db(BD_NAME);
+
+
+?>
